4afa91fc516dce642d0b27bd8a80d94960b96971
Dagostini01 <pedro01dagostini@gmail.com>
2024-09-17 15:46:24 -0300

    mudanças email gustavo

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -10,14 +10,14 @@ export async function POST(request: Request) {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER, // Variável de ambiente para o email
-      pass: process.env.EMAIL_PASS, // Variável de ambiente para a senha do app
+      user: "pedro01dagostini@gmail.com", // Variável de ambiente para o email
+      pass: "kdfk ovjw pevr hnbx", // Variável de ambiente para a senha do app
     },
   });
 
   const mailOptions = {
     from: process.env.EMAIL_USER, // Usar o email da variável de ambiente
-    to: 'gmota@cepamobilty.com', // Email de destino
+    to: 'gmota.bot@gmail.com', // Email de destino
     subject: 'Novo usuário cadastrado no sistema CEPA Mobility',
     text: `O usuário ${nome} com User ID: ${userId} foi cadastrado no sistema.`,
   };
